970c96adf117c2ec3c268f37a213448689fe8b65
Ndrsh <n0966910363@gmail.com>
2020-05-09 14:20:46 +0300

    додано кирилицю

--- src/hello/HelloWord.java
+++ src/hello/HelloWord.java
@@ -4,7 +4,8 @@ public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("Hello worlds");
 		
-		System.out.println("Hello worlds2");
+		System.out.println("Hello worlds2" + "привіт з України");
+		
 	}
 
 }
